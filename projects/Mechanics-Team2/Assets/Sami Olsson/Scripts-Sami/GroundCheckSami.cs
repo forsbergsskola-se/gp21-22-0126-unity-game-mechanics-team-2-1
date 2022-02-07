f1972b12d0c5418bf07d810e219287710168cdfe
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-02-07 21:19:17 +0100

    GroundCheck

--- projects/Mechanics-Team2/Assets/Sami Olsson/Scripts-Sami/GroundCheckSami.cs
+++ projects/Mechanics-Team2/Assets/Sami Olsson/Scripts-Sami/GroundCheckSami.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class GroundCheckSami : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool IsGrounded { get; private set; }
+    [SerializeField] private float groundCheckLength = 1f;
+    [SerializeField] private float groundCheckRadius = 0.5f;
+    [SerializeField] private LayerMask groundLayers;
+
+    private void Update()
     {
-        
+        CheckIfGrounded();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CheckIfGrounded()
     {
+        var ray = new Ray(transform.position, Vector3.down);
+        IsGrounded = Physics.SphereCast(ray, groundCheckRadius, groundCheckLength, groundLayers);
+
         
     }
 }
--- projects/Mechanics-Team2/Assets/Sami Olsson/Scripts-Sami/PlayerControllerSami.cs
+++ projects/Mechanics-Team2/Assets/Sami Olsson/Scripts-Sami/PlayerControllerSami.cs
@@ -1,12 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerSami : MonoBehaviour
 {
-   public Rigidbody myRigidbody;
+       public Rigidbody myRigidbody;
        public float moveSpeed = 5f;
        public float jumpForce = 500f;
+
+       [SerializeField] private Transform groundCheck;
+       [SerializeField] private LayerMask ground;
+      
    
        private void Update()
        {
@@ -24,7 +29,14 @@ public class PlayerControllerSami : MonoBehaviour
    
            //Apply jump force
            //Preferably interact with physics in FixedUpdate() 
-           if (jumpInput)
+           if (jumpInput && IsGrounden())
                myRigidbody.AddForce(Vector3.up * jumpForce);
        }
+
+       bool IsGrounden()
+       {
+           return Physics.CheckSphere(groundCheck.position, .1f, ground);
+       }
+
+     
 }
