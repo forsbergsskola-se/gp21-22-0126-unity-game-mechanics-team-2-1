e45ea1dbb7b51964fb050978ef3e3a44e332b568
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-02-09 00:34:12 +0100

    Update DashTeleportSami.cs

--- projects/Mechanics-Team2/Assets/Sami Olsson/Scripts-Sami/DashTeleportSami.cs
+++ projects/Mechanics-Team2/Assets/Sami Olsson/Scripts-Sami/DashTeleportSami.cs
@@ -7,4 +7,57 @@ using UnityEngine;
 public class DashTeleportSami : MonoBehaviour
 {
     
+    [SerializeField] Rigidbody myRigidbody;
+    
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private  float dashTimeCounter = 0f;
+    [SerializeField] private float chargeTime = 1f;
+    
+    
+    private bool canDash;
+    private bool canWalk;
+    
+    private float dashCharge;
+
+    private void Start()
+    {
+        canDash = true;
+        canWalk = true;
+    }
+
+    void Update()
+    {
+     
+        if (Input.GetKey(KeyCode.Q))
+        {
+            dashCharge += Time.deltaTime / chargeTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            dashCharge = Mathf.Clamp(dashCharge, 0f, 1f);
+            
+            StartCoroutine(dash(dashCharge));
+            Debug.Log(dashCharge);
+            dashCharge = 0f;
+        }
+    }
+    
+    
+    private IEnumerator dash(float input)
+    {
+        while (dashTimeCounter <= dashTime )
+        {
+            myRigidbody.velocity = new Vector3(dashSpeed * input, myRigidbody.velocity.y);
+            dashTimeCounter += Time.deltaTime;
+            yield return null;
+        }
+        myRigidbody.velocity = Vector3.zero;
+    
+        canDash = true;
+        dashTimeCounter = 0f;
+        Debug.Log(canDash);
+        yield return null;
+    }
 }
