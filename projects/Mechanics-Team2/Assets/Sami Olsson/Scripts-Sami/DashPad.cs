0007f60d2068a88cfc07a2b824fe483095bd903b
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-02-09 19:14:41 +0100

    Add dashpad

---
+++ projects/Mechanics-Team2/Assets/Sami Olsson/Scripts-Sami/DashPad.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashPad : MonoBehaviour
+{
+    private PlayerControllerSami _playerControllerSami;
+    private float currentMoveSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerControllerSami = gameObject.GetComponent<PlayerControllerSami>();
+        currentMoveSpeed = _playerControllerSami.moveSpeed;
+    }
+
+  
+    
+    private void OnCollisionEnter(Collision hit)
+    {
+
+        switch (hit.gameObject.tag)
+        {
+            case "DashBoost":
+                _playerControllerSami.moveSpeed = 25f;
+                break;
+            case "Ground":
+                _playerControllerSami.moveSpeed = currentMoveSpeed; 
+                   break;
+        }
+        
+    }
+}
