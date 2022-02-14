8f14cd34985f3b4b58de13d06c7f67cd9b8992d2
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2022-02-14 23:53:35 +0100

    Added FlyHooverEK script

---
+++ projects/Mechanics-Team2/Assets/Scripts/Efraim Kent/Fly/FlyHooverEK.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class FlyHooverEK : MonoBehaviour {
+    [SerializeField] float hooverDuration, hooverSpeed;
+    GameObject player;
+    Rigidbody playerRigidBody;
+    bool isHovering;
+
+    void Awake() {
+        player = GameObject.FindWithTag("Player");
+        playerRigidBody = player.GetComponent<Rigidbody>();
+        isHovering = false;
+    }
+
+    void Update() {
+        if (isHovering) {
+            Vector3 p = new Vector3(0, hooverSpeed * Time.deltaTime, 0);
+            player.transform.position += p;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Debug.Log(collision + " occured");
+            StartCoroutine(Hoover(collision));
+        }
+    }
+
+    IEnumerator Hoover(Collision collision) {
+
+        playerRigidBody.useGravity = false;
+        if (playerRigidBody.useGravity == false) {
+            Debug.Log("gravity is " + playerRigidBody.useGravity);
+        }
+        isHovering = true;
+
+        yield return new WaitForSeconds(hooverDuration);
+
+        playerRigidBody.useGravity = true;
+        if (playerRigidBody.useGravity) {
+            Debug.Log("gravity is " + playerRigidBody.useGravity);
+        }
+        isHovering = false;
+    }
+}
